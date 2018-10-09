fc462099633b9c959df4b4435064051b9b509f57
sibyldawn <sibyldawn@gmail.com>
2018-10-09 10:29:01 -0700

    Add Snapshot testing

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import App from './App';
+import { shallow } from 'enzyme';
+import renderer from 'react-test-renderer';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+
+it('matches the snapshot',() => {
+  const tree = renderer.create(<App />).toJSON()
+  expect(tree).toMatchSnapshot();
+})
